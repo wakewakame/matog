ab32d6a8a424ecd21ad3c761895e743a255838f3
wakewakame <0214.taiki@gmail.com>
2023-05-21 02:52:32 +0900

    eslint の追加と HTML のウェブコンポーネント化

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,21 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
-import wasmMVG from 'wasmMVG';
+import wasmMVG, { type Module } from 'wasmMVG'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+class HelloWolrd extends HTMLDivElement {
+  constructor () {
+    super()
+    this.className = 'app'
+    const text = document.createElement('h1')
+    this.appendChild(text)
+    wasmMVG()
+      .then((mvg: Module) => {
+        text.textContent = mvg.hello('world')
+      })
+      .catch((e: any) => {
+        console.error(e)
+      })
+  }
+}
+customElements.define('hello-world', HelloWolrd, { extends: 'div' })
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-
-const main = async () => {
-  const mvg = await wasmMVG();
-  console.log(mvg.hello("world"));
-};
-main();
+document.body.appendChild(document.createElement('div', { is: 'hello-world' }))
